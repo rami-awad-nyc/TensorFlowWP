bff2bffa4f0422ac19e1ce6c10b49d18d0a7a6dd
Rami Awad <rami.awad@nyu.edu>
2025-03-01 02:14:45 -0500

    setup the project

--- app.mjs
+++ app.mjs
@@ -1,1 +1,17 @@
+import express from 'express';
+import url from 'url';
+import path from 'path';
+import fs from 'fs';
 
+// setup & middleware
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const app = express();
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.render('layout.hbs');
+});
+
+app.listen(8080);
